9a27ed164c365a9acf4d7cb51337ece0fc9de5cd
Demid5 <demkozlov@yandex.ru>
2020-05-04 14:59:44 +0300

    added log4j properties

---
+++ src/main/java/music/Chord.java
@@ -0,0 +1,15 @@
+package music;
+
+public enum Chords {
+    Am, Em, Dm, Fm;
+
+    /*private String name;
+
+    Chords(String name) {
+        this.name = name;
+    }
+
+    public String getNameChord() {
+        return name;
+    }*/
+}
