04375a0965ab89a36d5b4ead18def29bdb08ed30
Demid5 <demkozlov@yandex.ru>
2020-05-04 18:22:31 +0300

    add minimal interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,36 @@
+import music.SoundPlayer;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+
+        SoundPlayer soundPlayer = new SoundPlayer();
+        System.out.println("Hello!");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter \"chord\" or \"chord, time\" or play(for play): ");
+            String answer = scanner.nextLine();
+            if (answer.equals("play")) {
+                soundPlayer.run();
+                break;
+            } else {
+                String[] chords = answer.split(",");
+                switch (chords.length) {
+                    case 1: {
+                        soundPlayer.addChord(chords[0]);
+                        break;
+                    }
+                    case 2: {
+                        soundPlayer.addChord(chords[0], Long.parseLong(chords[1].trim()));
+                        break;
+                    }
+                    default: {
+                        System.out.println("No correct data. I'm sorry");
+                    }
+                }
+            }
+        }
+
     }
 }
