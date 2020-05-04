db55ddc0e0ad14685cc709a0da171ce26eb67af2
Demid5 <demkozlov@yandex.ru>
2020-05-04 19:08:57 +0300

    change cmd interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,15 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         SoundPlayer soundPlayer = new SoundPlayer();
-        System.out.println("Hello!");
+        System.out.println("Hello! This \"Chords play game\".");
+        System.out.println("Example using:");
+        System.out.println("Enter \"chord\" or \"chord, time\" or play(for play): Am");
+        System.out.println("Enter \"chord\" or \"chord, time\" or play(for play): A, 4");
+        System.out.println("Enter \"chord\" or \"chord, time\" or play(for play): Dm");
+        System.out.println("Enter \"chord\" or \"chord, time\" or play(for play): play");
+        System.out.println(">>>>>>>>>>>>>");
+        System.out.println(">>>>>>>>>>>>>");
+        System.out.println("Start game:");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("Enter \"chord\" or \"chord, time\" or play(for play): ");
